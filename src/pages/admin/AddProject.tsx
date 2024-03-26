2b477907974b453c4e9850705d5692d9f12015ad
alfadlimaulana <alfadlimaulana@gmail.com>
2024-03-26 15:40:34 +0700

    fix: calendar error
    This reverts commit e8adeac720c9a03ba08664ec79120531aad339c5.

--- src/pages/admin/AddProject.tsx
+++ src/pages/admin/AddProject.tsx
@@ -276,7 +276,7 @@ function AddProject() {
                             </FormControl>
                           </PopoverTrigger>
                           <PopoverContent className="w-auto p-0" align="start">
-                            <Calendar mode="single" selected={field.value ?? []} onSelect={field.onChange} initialFocus />
+                            <Calendar mode="single" selected={field.value ?? undefined} onSelect={field.onChange} initialFocus />
                           </PopoverContent>
                         </Popover>
                         <FormMessage />
